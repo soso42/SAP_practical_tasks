c6a05a0c36d03a55105acda5b5344d0209e00aa2
Soso Janashvili <sosojanashvili@gmail.com>
2025-11-07 00:23:20 +0400

    Refactored naming

---
+++ 1.Manifest_project/src/main/java/com/example/javatutorial/exception/NotAuthorizedException.java
@@ -0,0 +1,4 @@
+package com.example.javatutorial.exception;
+
+public class NotAuthorizedException {
+}
---
+++ 1.Manifest_project/src/main/java/com/example/javatutorial/security/WebSecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.javatutorial.security;
+
+public class WebSecurityConfig {
+}
