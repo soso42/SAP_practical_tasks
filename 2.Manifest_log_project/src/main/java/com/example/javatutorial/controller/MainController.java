6d8d376727a11baaa20ca78c12cd8e4a355a807c
Soso Janashvili <sosojanashvili@gmail.com>
2025-11-08 19:20:14 +0400

    Added logging-service

--- 2.Manifest_log_project/src/main/java/com/example/javatutorial/controller/MainController.java
+++ 2.Manifest_log_project/src/main/java/com/example/javatutorial/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.example.javatutorial.controller;
 
 import com.example.javatutorial.exception.NotAuthorizedException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,13 +16,17 @@ import java.util.Map;
 
 @RestController
 @RequestMapping(path = "")
+@Slf4j
 public class MainController {
 
     @GetMapping(path = "")
     public ResponseEntity<?> readAll(@AuthenticationPrincipal Token token) {
         if (!token.getAuthorities().contains(new SimpleGrantedAuthority("Display"))) {
+            log.info("User {} was denied entry", token.getEmail());
             throw new NotAuthorizedException("This operation requires \"Display\" scope");
         }
+
+        log.info("User {} was logged in", token.getEmail());
 
         Map<String, String> result = new HashMap<>();
         result.put("Logged in user authorities", token.getAuthorities().toString());
