78decb9d02c2371d3d985beff184f104de7ed27b
Soso Janashvili <sosojanashvili@gmail.com>
2025-11-12 23:06:49 +0400

    Added hana, repository layer, service layer

---
+++ 5.Hana_cloud/product-service/src/main/java/com/example/productservice/entity/Product.java
@@ -0,0 +1,26 @@
+package com.example.productservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private Integer price;
+
+}
